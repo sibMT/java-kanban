f3c49d66afb431792cdfe4a46254e69c40ead860
Tarasov Maxim <tarasovmaxxx@yandex.ru>
2025-02-01 13:45:56 +0700

    Реализация тз, спринт 6 с учетом стиля(7 вариант)

--- src/controllers/HistoryManager.java
+++ src/controllers/HistoryManager.java
@@ -1,10 +1,12 @@
 package controllers;
 
 import classes.Task;
+
 import java.util.List;
 
 public interface HistoryManager {
     void addToHistory(Task task);
+
     void remove(int id);
 
     List<Task> getHistory();
