4fcdedd48925a2da5bff5fe717c785bb5d81a8cb
Tarasov Maxim <tarasovmaxxx@yandex.ru>
2025-02-03 12:50:54 +0700

    Реализация тз, спринт 6 с учетом стиля(10 вариант)

--- src/controllers/InMemoryHistoryManager.java
+++ src/controllers/InMemoryHistoryManager.java
@@ -20,6 +20,7 @@ public class InMemoryHistoryManager implements HistoryManager {
         public Node<Task> prev;
         public Node<Task> next;
         public Task data;
+        //комментарий для коммита,проверка ветки
 
         public Node(Node<Task> prev, Node<Task> next, Task data) {
             this.prev = prev;
