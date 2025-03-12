e091312a0701d31c81e5143e7e0137a5c8d912b3
Tarasov Maxim <tarasovmaxxx@yandex.ru>
2025-03-12 18:30:50 +0700

    Спринт 8(c доработками)

--- src/controllers/Managers.java
+++ src/controllers/Managers.java
@@ -6,8 +6,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Managers {
 
